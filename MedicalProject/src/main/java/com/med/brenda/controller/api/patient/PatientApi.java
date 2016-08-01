e89f19ecb528413259d4aad595b95bfa5ab404f1
陈鸿健 <13552666934@139.com>
2016-08-01 19:27:48 +0800

    加

--- MedicalProject/src/main/java/com/med/brenda/controller/api/patient/PatientApi.java
+++ MedicalProject/src/main/java/com/med/brenda/controller/api/patient/PatientApi.java
@@ -10,9 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSON;
 import com.med.brenda.exception.BusinessException;
 import com.med.brenda.model.Hzxx;
+import com.med.brenda.model.User;
 import com.med.brenda.service.IHzxxService;
+import com.med.brenda.service.IUserService;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
 
 /**
  * 患者的端Api<一>
@@ -20,7 +23,7 @@ import com.wordnik.swagger.annotations.ApiParam;
  * <p>Title: PatientApi.java</p>
  * <p>Description:$</p>
  * <p>Package:com.med.brenda.controller.api.patient</p>
- * @author bgly
+ * @author chenhj(brenda)
  * @date 2016年8月1日 下午4:02:40
  * @tag 
  * @encode
@@ -31,15 +34,17 @@ public class PatientApi {
 	
 	@Autowired
 	private IHzxxService hzxxService;
+	@Autowired
+	private IUserService userService;
 	/**
-	 * 根据用户名获取用户对象
+	 * 根据用户ID获取用户对象
 	 * @param name
 	 * @return
 	 */
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	@ApiOperation(value = "根据用户患者ID获取患者对象", httpMethod = "GET", response = Hzxx.class, notes = "根据用户患者ID获取患者对象")
-	public String getUserByName(@ApiParam(required = true, name = "id", value = "患者主键(long类型)") @PathVariable long id) throws Exception{
+	public String getHzById(@ApiParam(required = true, name = "id", value = "患者主键(long类型)") @PathVariable long id) throws Exception{
 		
 		Hzxx hz = hzxxService.findHzByHzID(id);
 
@@ -47,6 +52,28 @@ public class PatientApi {
 	    	return JSON.toJSONString(hz);
 	    } else {
 	        throw new BusinessException("根据{id=" + id + "}获取不到Hzxx对象");
+	    }
+	}
+	
+	/**
+	 * 根据用户名+密码获取用户对象
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping(value="/{username}/{password}", method = RequestMethod.POST)
+	@ResponseBody
+	@ApiOperation(value = "根据用户名+密码获取用户对象", httpMethod = "POST", response = User.class, notes = "根据用户名+密码获取用户对象")
+	public String getUser(@ApiParam(required = true, name = "username", value = "用户名称") @PathVariable String username, 
+			@ApiParam(required = true, name = "password", value = "用户密码MD5转大写") @PathVariable String password) throws Exception{
+		User puser = new User();
+		puser.setUsername(username);
+		puser.setPassword(password);
+		User newUser = userService.getUserByUserNamePwd(puser);
+
+	    if(newUser != null) {
+	    	return JSON.toJSONString(newUser);
+	    } else {
+	        throw new BusinessException("根据{name=" + username + ", password="+ password +"}获取不到User对象");
 	    }
 	}
 }
