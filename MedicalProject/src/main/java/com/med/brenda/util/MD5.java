35d8ca78092b29f42af3b1f201386b6f9146431e
陈鸿健 <13552666934@139.com>
2017-02-06 14:21:21 +0800

    modify md5

--- MedicalProject/src/main/java/com/med/brenda/util/MD5.java
+++ MedicalProject/src/main/java/com/med/brenda/util/MD5.java
@@ -66,7 +66,7 @@ public class MD5 {
 
     public static void main(String[] args) {
         MD5 getMD5 = new MD5();//371322
-        System.out.println(getMD5.GetMD5Code("210211"));
+        System.out.println(getMD5.GetMD5Code("110101"));
         //D0970714757783E6CF17B26FB8E2298F
     }
 }
